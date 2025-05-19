72b0394cb39540bcedc8a20f29e0d8067067af95
nsylke <hello@nsylke.me>
2025-05-19 17:17:43 -0500

    ci: add chrome options to accessibility workflow

--- scripts/run-a11y-tests.mjs
+++ scripts/run-a11y-tests.mjs
@@ -1,5 +1,7 @@
 import { execSync } from "node:child_process";
-import { readFileSync } from "node:fs";
+import { readFileSync, mkdtempSync } from "node:fs";
+import {join} from 'node:path';
+import {tmpdir} from 'node:os';
 
 // Inspired by the following article, with modifications:
 // https://medium.com/@SkorekM/from-theory-to-automation-wcag-compliance-using-axe-core-next-js-and-github-actions-b9f63af8e155
@@ -8,13 +10,15 @@ try {
     .split("\n")
     .filter((path) => path.trim() !== "");
 
+  const tempDataDir = mkdtempSync(join(tmpdir(), 'axe-user-data-'))
+
   let hasFailures = false;
 
   for (const url of urls) {
     console.log(`Testing: ${url}`);
 
     try {
-      execSync(`pnpx @axe-core/cli "${url}" --exit`, { stdio: "inherit" });
+      execSync(`pnpx @axe-core/cli "${url}" --chrome-option="--user-data-dir=${tempDataDir}" --exit`, { stdio: "inherit" });
     } catch (error) {
       console.log("Accessibility issues found on this page");
       hasFailures = true;
