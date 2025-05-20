1708f1d0f068515d2a48a0a4c1c923197acfd042
nsylke <hello@nsylke.me>
2025-05-19 19:24:09 -0500

    chore: cleanup unused code comments in a11y script

--- scripts/run-a11y-tests.mjs
+++ scripts/run-a11y-tests.mjs
@@ -1,6 +1,5 @@
 import { execSync } from "node:child_process";
 import { readFileSync } from "node:fs";
-// import { resolve } from "node:path";
 
 // Inspired by the following article, with modifications:
 // https://medium.com/@SkorekM/from-theory-to-automation-wcag-compliance-using-axe-core-next-js-and-github-actions-b9f63af8e155
@@ -8,9 +7,6 @@ try {
   const urls = readFileSync("./scripts/sitemap-urls.txt", "utf8")
     .split("\n")
     .filter((path) => path.trim() !== "");
-
-  // const binPath = execSync("pnpm bin").toString().trim();
-  // const chromedriverPath = resolve(binPath, "chromedriver");
 
   let hasFailures = false;
 
