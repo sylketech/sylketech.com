23f13f02801859a5735ef0a1420cb5802f0a45e5
nsylke <hello@nsylke.me>
2025-05-19 21:14:09 -0500

    refactor: update robots.txt to use base url env

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
         userAgent: "*",
       },
     ],
-    sitemap: "https://sylketech.com/sitemap.xml",
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
   };
 }
