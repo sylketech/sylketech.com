22336c98639840e54a04b235d5cf9774ee242729
Nicholas Sylke <hello@nsylke.me>
2025-05-20 22:47:52 -0500

    feat: add theme selector functionality (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="bg-brand-graphite px-8 py-6 h-dvh flex items-center justify-center">
+    <main className="bg-brand-pale-white dark:bg-brand-graphite px-8 py-6 h-dvh flex items-center justify-center">
       <div className="max-w-xl space-y-6">
-        <h1 className="flex flex-col md:flex-row text-3xl font-semibold leading-7 select-none text-brand-pale-white">
+        <h1 className="flex flex-col md:flex-row text-3xl font-semibold leading-7 select-none text-brand-graphite dark:text-brand-pale-white">
           SYLKE TECH
         </h1>
-        <p className="text-gray-300">
+        <p className="text-gray-700 dark:text-gray-300">
           Free and open source building blocks for secure, privacy-first web applications.
         </p>
         <Link
